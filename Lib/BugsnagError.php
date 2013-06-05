18b4e0d84429f82816ae18705d5fa9c61233f630
Craig Morris <craig.morris@vml.com.au>
2013-06-05 10:14:32 +1000

    Renaming lib to Lib

---
+++ Lib/BugsnagError.php
@@ -0,0 +1,25 @@
+<?php
+
+App::import('Vendor', 'Bugsnag', array('file' => 'bugsnag' . DS . 'bugsnag' . DS . 'lib'. DS . 'bugsnag.php'));
+Bugsnag::register(Configure::read('Bugsnag.apiKey'));
+
+class BugsnagError extends ErrorHandler
+{
+    public static function handleError($code, $description, $file = null, $line = null, $context = null) 
+    {
+    	// Call Bugsnag
+		Bugsnag::errorHandler($code, $description, $file, $line, $context);
+        
+        // Fall back to cake
+        return parent::handleError($code, $description, $file, $line, $context);
+    }
+    
+    public static function handleException(Exception $exception) 
+    {
+    	// Call Bugsnag
+		Bugsnag::exceptionHandler($exception);
+
+    	// Fall back to Cake..
+		return parent::handleException($exception);
+    }
+}
